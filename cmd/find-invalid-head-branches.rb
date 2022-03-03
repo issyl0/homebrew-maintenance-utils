75db1bb8377bbb905883fa9190fde5093a34f970
Issy Long <me@issyl0.co.uk>
2022-03-03 00:40:08 +0000

    Report on GitHub-hosted formulae with invalid HEAD branches
    - I used to have an ugly shell command to scan and generate `git clone` lines:
    
    ```shell
    git -C $(brew --repo homebrew/core) grep "head \"" | grep git | awk -F'head "' '{ print $2 }' | awk -F', branch: ' '{ print "git clone", $1, "-b", $2 }' | sed 's/"//g'
    ```
    
    - I would then paste those `git clone` lines and clone a selection of repos (A
      letters, Z letters, etc.) and note the ones that errored with "no branch
      exists".
    - Then, I'd find the corresponding formula and edit it with the current default
      branch (for example, `main` instead of `master`). This meant that users
      installing `--HEAD` of a formula they like would be able to do so!
    - This was very manual, plus cloning all the repos only to delete them again
      was wasteful.
    
    ---
    
    - With this script, I can run `brew find-invalid-head-branches` and, **for
      GitHub-hosted formulae only** (at the moment), have a list of formulae with
      HEAD branches that don't exist on the remote.
    - A future goal with this is to have a switch that makes the changes to the
      `head "url", branch: "name"` line in the corresponding formula.
    
    ---
    
    - This does use a new method, `GitHub.branch_exists?`, which I've not
      upstreamed into Homebrew's `utils/github.rb` yet.

---
+++ cmd/find-invalid-head-branches.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require "cli/parser"
+require "formula"
+require "utils/github"
+
+module Homebrew
+  module_function
+
+  def find_invalid_head_branches_args
+    Homebrew::CLI::Parser.new do
+      usage_banner <<~EOS
+        `find-invalid-head-branches`
+        Find formulae whose specified HEAD branch doesn't exist on the remote repo.
+      EOS
+
+      named_args max: 0
+    end
+  end
+
+  def find_invalid_head_branches
+    odie "Set the `HOMEBREW_GITHUB_API_TOKEN envvar." if ENV["HOMEBREW_GITHUB_API_TOKEN"].blank?
+
+    head_formulae.each do |formula|
+      # TODO: Support GitLab and other code hosts.
+      next unless formula.head.url.include?("github.com")
+
+      formula_head_branch = formula.head.specs[:branch]
+      next if formula_head_branch.nil? # Lots of `head do` blocks have `nil` branch data.
+
+      odebug "Scanning #{formula.name}..."
+      user, repo = parse_repo_url(formula.head.url)
+      remote_default_branch = GitHub.repository(user, repo)["default_branch"]
+      next if formula_head_branch == remote_default_branch
+      # A branch is still valid if it exists on the repo but is not the default.
+      next if GitHub.branch_exists?(user, repo, formula_head_branch)
+
+      opoo "#{formula.name}: #{formula_head_branch} => #{remote_default_branch}"
+    end
+  end
+
+  def head_formulae
+    Formula.all.reject { |f| f.head.nil? }
+  end
+
+  def parse_repo_url(url)
+    url.gsub("https://", "").gsub("github.com/", "").gsub(".git", "").split("/")
+  end
+end
