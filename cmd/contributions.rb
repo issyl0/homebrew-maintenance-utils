0b7baab110bfaee13a25bc1e1479ec30e76a818c
Issy Long <me@issyl0.co.uk>
2022-07-23 01:13:47 +0100

    cmd/contributions: Refactoring the `git log` portion

--- cmd/contributions.rb
+++ cmd/contributions.rb
@@ -14,7 +14,7 @@ module Homebrew
         description: "The GitHub username of the user whose contributions you want to find."
 
       flag "--email",
-        description: "A user's email address that they commit with (useful if not public on GitHub)."
+        description: "A user's email address that they commit with."
 
       flag "--from=",
         description: "Date (ISO-8601 format) to start searching contributions."
@@ -26,12 +26,18 @@ module Homebrew
         description: "The Homebrew repository to search for contributions in (brew, core, cask, bundle, ...)."
 
       conflicts "--username", "--email"
-      named_args max: 0
+
+      named_args :none
     end
   end
 
   def contributions
     args = contributions_args.parse
+
+    if !args[:repo] && (args[:username] || args[:email])
+      ofail "`--repo` and one of `--username` or `--email` are required."
+      return
+    end
 
     repo_location = find_repo_path_for_repo(args[:repo])
     unless repo_location
@@ -39,13 +45,8 @@ module Homebrew
       return
     end
 
-    if args[:from] && args[:to]
-      commits = `git -C #{repo_location} log --oneline --author=#{args[:username] || args[:email]} --before=#{args[:to]} --after=#{args[:from]} | wc -l`.strip.to_i
-      coauthorships = `git -C #{repo_location} log --oneline --format="%(trailers:key=Co-authored-by:)" --before=#{args[:to]} --after=#{args[:from]} | grep #{args[:username] || args[:email]} | wc -l`.strip.to_i
-    else
-      commits = `git -C #{repo_location} log --oneline --author=#{args[:username] || args[:email]}  | wc -l`.strip.to_i
-      coauthorships = `git -C #{repo_location} log --oneline --format="%(trailers:key=Co-authored-by:)" | grep #{args[:username] || args[:email]} | wc -l`.strip.to_i
-    end
+    commits = git_log_cmd("author", repo_location, args)
+    coauthorships = git_log_cmd("coauthorships", repo_location, args)
 
     sentence = "Person #{args[:username] || args[:email]} directly authored #{commits} commits and co-authored #{coauthorships} commits to #{args[:repo]}"
     sentence += args[:from] && args[:to] ? " between #{args[:from]} and #{args[:to]}." : " in all time."
@@ -64,5 +65,15 @@ module Homebrew
     when "bundle"
       "#{HOMEBREW_REPOSITORY}/Library/Taps/homebrew/homebrew-bundle"
     end
+  end
+
+  def git_log_cmd(kind, repo_location, args)
+    cmd = "git -C #{repo_location} log --oneline"
+    cmd += " --author=#{args[:username] || args[:email]}" if kind == "author"
+    cmd += " --format='%(trailers:key=Co-authored-by:)'" if kind == "coauthorships"
+    cmd += " --before=#{args[:to]} --after=#{args[:from]}" if args[:from] && args[:to]
+    cmd += " | grep #{args[:username] || args[:email]}" if kind == "coauthorships"
+
+    `#{cmd} | wc -l`.strip.to_i
   end
 end
